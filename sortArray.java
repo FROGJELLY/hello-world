9d2bb5f7a24b9e1b37ffe7c9919c6d446c4f61ec
Teerada <teerada.chat@gmail.com>
2025-04-11 21:58:42 +0700

    Update sortArray.java

--- sortArray.java
+++ sortArray.java
@@ -1,22 +1,24 @@
-public class x {
+public class SortArray {
 
-    static void y(int[] a) {
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                if (a[i] < a[j]) { // BUG: should be a[i] > a[j] for ascending
-                    int t = a[i];
-                    a[i] = a[j];
-                    a[j] = t;
+    // Method to sort array in ascending order
+    static void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] < arr[j]) { // swap if smaller
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
     }
 
-    public static void main(String[] b) {
-        int[] a = {3, 2, 1};
-        y(a);
-        for (int i = 0; i <= a.length; i++) { // BUG: should be i < a.length
-            System.out.print(a[i] + " ");
+    public static void main(String[] args) {
+        int[] numbers = {3, 2, 1};
+        sort(numbers);
+
+        for (int i = 0; i < numbers.length; i++) { // fixed loop condition
+            System.out.print(numbers[i] + " ");
         }
     }
 }
